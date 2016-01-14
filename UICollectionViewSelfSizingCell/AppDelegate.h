e50e48ece265b3014670989d7faa2fc34d2b4d02
deepakbhati99 <Khatri@khatris-mac-mini.local>
2016-01-14 11:48:34 +0530

    Initial Commit

---
+++ UICollectionViewSelfSizingCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewSelfSizingCell
+//
+//  Created by Himanshu Khatri on 1/14/16.
+//  Copyright © 2016 bd 001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
